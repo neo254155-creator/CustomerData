5eb721aa996adcdd5d568f2c27fd523f71560ec9
neo254155-creator <neo254155@gmail.com>
2025-11-02 20:18:34 +0530

    add package

--- CHW.java
+++ CHW.java
@@ -1,12 +1,22 @@
-import java .io.CharArrayWriter;
+import java.io.CharArrayWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class CHW {
-    public static void main (String args[])
-    {
-        CharArrayWriter cr=new CharArrayWriter();
-        cr.write('a');
-        cr.write('b');  
-        cr.writeTo("C:\\Users\\Aditya\\STD_B.txt");
-        cr.close();
+    public static void main(String[] args) {
+        try {
+            CharArrayWriter cr = new CharArrayWriter();
+            cr.write('a');
+            cr.write('b');
+
+            // Wrap the file path in a FileWriter
+            FileWriter fw = new FileWriter("C:\\Users\\Aditya\\STD_B.txt");
+            cr.writeTo(fw);
+
+            fw.close();
+            cr.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
-    
 }
