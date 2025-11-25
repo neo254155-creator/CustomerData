24e49097ff0722d99eac7a13fc3bff0d93304e8a
neo254155-creator <neo254155@gmail.com>
2025-11-25 20:53:53 +0530

    change line

--- CustomerData.java
+++ CustomerData.java
@@ -19,7 +19,7 @@ public class CustomerData implements Serializable{
        
     }
     public String toString(){
-        return "Customer ID:" + custID + " Customer Name:" + custName + " Phone Number:" + phNO;
+        return "Customer ID:" + custID + " \n Customer Name:" + custName + " \n Phone Number:" + phNO;
     }
     public static void main (String args[])throws Exception{
         CustomerData List[]={new CustomerData("C001","VIRAT KOHLI",9876543210L),new CustomerData("C002","ADITYA",8765432109L),new CustomerData("C003","STEVE SMITH",7654321098L)};
@@ -36,7 +36,8 @@ public class CustomerData implements Serializable{
         java.util.Scanner sc = new java.util.Scanner(System.in);
         FileInputStream fis=new FileInputStream("C:\\Users\\Aditya\\STD_B.txt");
         ObjectInputStream ois=new ObjectInputStream(fis);
-        int length =ois.readInt();
+       int length =ois.readInt ();
+       
         CustomerData list[]=new CustomerData[length];
         for(int i=0;i<length;i++){
             list [i]=(CustomerData)ois.readObject();
@@ -47,11 +48,14 @@ public class CustomerData implements Serializable{
             if(list[i].custName.equalsIgnoreCase(name)){
              System.out.println(list[i]);
             }
+            System.out.println();
         }
         ois.close();
         fis.close();
+
         sc.close();
     }
+
 }
 
 
